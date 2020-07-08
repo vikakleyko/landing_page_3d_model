650e231beff75a35910c8c3d82dec3d5695bcf3d
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-07-08 09:57:24 -0700

    calculator

--- script/script.js
+++ script/script.js
@@ -282,7 +282,6 @@ window.addEventListener("DOMContentLoaded", () => {
     const target = event.target;
     const regExp = new RegExp("^\\d+$");
     let isValid = regExp.test(target.value);
-    console.log(warning);
     if (!isValid && target.value) {
       warning.textContent = "*numbers only allowed";
       warning.style.fontSize = "12px";
@@ -305,5 +304,50 @@ window.addEventListener("DOMContentLoaded", () => {
   calcSquare.addEventListener("input", validate);
   calcCount.addEventListener("input", validate);
   calcDay.addEventListener("input", validate);
+
+
+  // calculator
+  const calc = (price = 100) => {
+      const calcBlock = document.querySelector(".calc-block"),
+      calcType = document.querySelector(".calc-type"),
+      calcSquare = document.querySelector(".calc-square"),
+      calcDay = document.querySelector(".calc-day"),
+      calcCount = document.querySelector(".calc-count"),
+      totalValue = document.getElementById("total");
+
+      const countSum = () => {
+        let total = 0,
+        countValue = 1,
+        dayValue = 1;
+
+        const typeValue = calcType.options[calcType.selectedIndex].value,
+          squareValue = +calcSquare.value;
+
+          if (calcCount.value > 1) {
+            countValue += (calcCount.value - 1) / 10;
+            console.log(calcCount.value);
+          }
+
+          if (calcDay.value && calcDay.value < 5) {
+              dayValue *= 2;
+          } else if (calcDay.value && calcDay.value < 10) {
+            dayValue *= 1.5;
+          }
+
+          if (typeValue && squareValue) {
+            total = price * squareValue * typeValue * countValue * dayValue;
+          }
+          totalValue.textContent = Math.floor(total);
+      };
+
+      calcBlock.addEventListener("change", event => {
+        const target = event.target;
+        if (target.matches("input") || target.matches("select")) {
+          countSum();
+        }
+      });
+  };
+
+  calc(100);
 
 });
